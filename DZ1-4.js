c3d6bc72ea3bab667c9cff4960b6c2e5528b9ef2
Unknown <nikshes.ru@gmail.com>
2017-02-09 21:53:15 +0300

    fix

--- DZ1-4.js
+++ DZ1-4.js
@@ -1,50 +1,66 @@
 "use strict";
 
-console.log ("hi")
+console.log ("hi DZ1-4")
 
 var a=0;
 var b=0;
 
-
-
+// Задача №1
 function triangle() {
 	for (a="#"; a.length<=7; a=a+"#"){
 		console.log (a);
 	}
 }	
 
+// Задача №2
 function FizzBuzz () {
 	for (a=0; a<=100; a++){
-		if (a%3 == 0){
-			console.log ("Fizz");
-			continue;
-		}
-		if (a%5 == 0){
-			console.log ("Buzz");
-			continue;
-		}
-		console.log (a)
-		}
-}	
-
-function FizzBuzz2 () {
-	for (a=0; a<=100; a++){
 		if ((a%3 == 0) && (a%5 == 0)){
-			console.log ("FizzBuzz");
-			continue;
+		console.log ("FizzBuzz");
+		continue;
+		}
+		else {
+			if (a%3 == 0){
+				console.log ("Fizz");
+				continue;
+			}
+			if (a%5 == 0){
+				console.log ("Buzz");
+				continue;
+			}
 		}
 		console.log (a)
 		}
 }	
 
+// function FizzBuzz2 () {
+// 	for (a=0; a<=100; a++){
+// 		if ((a%3 == 0) && (a%5 == 0)){
+// 			console.log ("FizzBuzz");
+// 			continue;
+// 		}
+// 		console.log (a)
+// 		}
+// }	
+
+// Задача №3 
 function chessboard() {
-	for (a="# # # # "; a.length<=8; a=a+" \n"){
-		console.log (a);
-
+	var a = "# # # # ";
+	var b = " # # # #";
+	var c;
+	for (c=0; c<8;c++){
+		if (c%2 == 0) {
+			console.log (a)
+		}
+ 		else {
+		 	console.log (b);
+		} 
+			
+		
 	}
+}
 
-
-}	
+// Задача №3  (решение учебника)	
 function chessboard2() {
 var size = 8;
 
@@ -63,6 +79,7 @@ for (a = 0; a < size; a++) {
 console.log(board);
 }
 
-function min (a,b) {
-	return Math.min (a,b);		
+// Задача №4
+function min (a,b){
+	return a < b ? a : b;
 }
